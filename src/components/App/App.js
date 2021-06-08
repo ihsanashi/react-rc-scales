df396688b7f06b0419db246dd07293c1a08e48f3
Ahmad Ridzwan Bin Zainuddin <ahmadridzwan91@gmail.com>
2021-06-08 15:38:33 +0800

    Make values dynamic, added data population, and dynamic scrolling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './App.module.css';
 import { useForm } from 'react-hook-form';
 import { calculateAxleLoad } from '../services/calcService';
 
 export default function App() {
+  let result = {};
+  const [balance, setBalanceObj] = useState(result);
+  const [showBalance, setShowBalance] = useState(false);
+
+  const showInputRef = useRef();
+  const showBalanceRef = useRef();
+
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
-    let result = calculateAxleLoad(data);
-    console.log('result', result);
+    // Calculate balance values
+    result = calculateAxleLoad(data);
+    setBalanceObj(result);
+    setShowBalance(true);
+    scrollTo(showBalanceRef);
+  };
+
+  const resetValues = () => {
+    // Scroll to weight input
+    scrollTo(showInputRef);
+    // Reset form
+    reset();
+    // Hide balance displays
+    // 500ms timeout to enable scroll to work properly
+    setTimeout(function(){ 
+      setShowBalance(false);
+    }, 10);
+  };
+
+  const scrollTo = (ref) => {
+    if (ref) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
   };
 
   return (
     <main className={styles.main}>
-      <section className={styles.car__image}>
+      <section className={styles.car__image} ref={ showInputRef }>
         <img src='./rc-car.svg' alt='RC car' />
       </section>
       <section>
@@ -28,7 +57,7 @@ export default function App() {
               <label className={styles.label}>Front left</label>
               <input
                 className={styles.input__value}
-                placeholder='333.8g'
+                placeholder='FL'
                 {...register('frontLeft', { required: true })}
               />
               {errors.frontLeft && 'Front Left value is required!'}
@@ -37,7 +66,7 @@ export default function App() {
               <label className={styles.label}>Front right</label>
               <input
                 className={styles.input__value}
-                placeholder='325.6g'
+                placeholder='FR'
                 {...register('frontRight', { required: true })}
               />
               {errors.frontRight && 'Front Right value is required!'}
@@ -49,7 +78,7 @@ export default function App() {
               <label className={styles.label}>Rear left</label>
               <input
                 className={styles.input__value}
-                placeholder='334.7g'
+                placeholder='RL'
                 {...register('rearLeft', { required: true })}
               />
               {errors.rearLeft && 'Rear Left value is required!'}
@@ -58,58 +87,65 @@ export default function App() {
               <label className={styles.label}>Front right</label>
               <input
                 className={styles.input__value}
-                placeholder='335.5g'
+                placeholder='RR'
                 {...register('rearRight', { required: true })}
               />
               {errors.rearRight && 'Rear Right value is required!'}
             </div>
           </div>
+          <button type="submit">Submit</button>
         </form>
       </section>
-      <section className={styles.total__weight}>
-        <h5>total weight</h5>
-        <h2>1329.6g</h2>
+
+      { showBalance &&
+      <section ref={ showBalanceRef }>
+        <section className={styles.total__weight}>
+          <h5>total weight</h5>
+          <h2>{ balance.totalWeight }g</h2>
+        </section>
+        <section className={styles.weight__balance}>
+          <h2 className={styles.section__title}>weight balance</h2>
+          <div className={styles.grid__container}>
+            <div className={styles.input__container}>
+              <label className={styles.label}>Front</label>
+              <h4 className={styles.weight__balance__value}>{ balance.frontBalance }g</h4>
+              <h2 className={styles.weight__balance__percentage}>{ balance.frontBalancePCT }%</h2>
+            </div>
+            <div className={styles.input__container}>
+              <label className={styles.label}>rear</label>
+              <h4 className={styles.weight__balance__value}>{ balance.rearBalance }g</h4>
+              <h2 className={styles.weight__balance__percentage}>{ balance.rearBalancePCT }%</h2>
+            </div>
+            <div className={styles.input__container}>
+              <label className={styles.label}>left</label>
+              <h4 className={styles.weight__balance__value}>{ balance.leftBalance }g</h4>
+              <h2 className={styles.weight__balance__percentage}>{ balance.leftBalancePCT }%</h2>
+            </div>
+            <div className={styles.input__container}>
+              <label className={styles.label}>right</label>
+              <h4 className={styles.weight__balance__value}>{ balance.rightBalance }g</h4>
+              <h2 className={styles.weight__balance__percentage}>{ balance.rightBalancePCT }%</h2>
+            </div>
+          </div>
+        </section>
+        <section className={styles.diagonal__balance}>
+          <h2 className={styles.section__title}>diagonal balance</h2>
+          <div className={styles.grid__container}>
+            <div className={styles.input__container}>
+              <label className={styles.label}>FL + RR</label>
+              <h4 className={styles.weight__balance__value}>{ balance.flrrBalance }g</h4>
+              <h2 className={styles.weight__balance__percentage}>{ balance.flrrBalancePCT }%</h2>
+            </div>
+            <div className={styles.input__container}>
+              <label className={styles.label}>FR + RL</label>
+              <h4 className={styles.weight__balance__value}>{ balance.frrlBalance }g</h4>
+              <h2 className={styles.weight__balance__percentage}>{ balance.frrlBalancePCT }%</h2>
+            </div>
+          </div>
+        </section>
+        <button type="button" onClick={() => resetValues()}>Reset Values</button>
       </section>
-      <section className={styles.weight__balance}>
-        <h2 className={styles.section__title}>weight balance</h2>
-        <div className={styles.grid__container}>
-          <div className={styles.input__container}>
-            <label className={styles.label}>Front</label>
-            <h4 className={styles.weight__balance__value}>659.4g</h4>
-            <h2 className={styles.weight__balance__percentage}>50%</h2>
-          </div>
-          <div className={styles.input__container}>
-            <label className={styles.label}>rear</label>
-            <h4 className={styles.weight__balance__value}>670.2g</h4>
-            <h2 className={styles.weight__balance__percentage}>50%</h2>
-          </div>
-          <div className={styles.input__container}>
-            <label className={styles.label}>left</label>
-            <h4 className={styles.weight__balance__value}>668.5g</h4>
-            <h2 className={styles.weight__balance__percentage}>50%</h2>
-          </div>
-          <div className={styles.input__container}>
-            <label className={styles.label}>right</label>
-            <h4 className={styles.weight__balance__value}>661.1g</h4>
-            <h2 className={styles.weight__balance__percentage}>50%</h2>
-          </div>
-        </div>
-      </section>
-      <section className={styles.diagonal__balance}>
-        <h2 className={styles.section__title}>diagonal balance</h2>
-        <div className={styles.grid__container}>
-          <div className={styles.input__container}>
-            <label className={styles.label}>FL + RR</label>
-            <h4 className={styles.weight__balance__value}>660.3g</h4>
-            <h2 className={styles.weight__balance__percentage}>50%</h2>
-          </div>
-          <div className={styles.input__container}>
-            <label className={styles.label}>FR + RL</label>
-            <h4 className={styles.weight__balance__value}>669.3g</h4>
-            <h2 className={styles.weight__balance__percentage}>50%</h2>
-          </div>
-        </div>
-      </section>
+      }
     </main>
   );
 }
